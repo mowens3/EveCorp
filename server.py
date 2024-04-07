db3bb0d3506d2bc1db74d10ba551970630500c84
Matt Owens <mowens3@gmail.com>
2024-04-06 23:29:48 -0700

    updates to server to run bot and app

--- server.py
+++ server.py
@@ -3,17 +3,20 @@ import http.server
 import socketserver
 
 from http import HTTPStatus
+from commissar.app import app
+
+# class Handler(http.server.SimpleHTTPRequestHandler):
+#     def do_GET(self):
+#         self.send_response(HTTPStatus.OK)
+#         self.end_headers()
+#         msg = 'Hello! you requested %s' % (self.path)
+#         self.wfile.write(msg.encode())
 
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+# port = int(os.getenv('PORT', 80))
+# print('Listening on port %s' % (port))
+# httpd = socketserver.TCPServer(('', port), Handler)
+# httpd.serve_forever()
 
-
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+app.start()
+bot.start()
