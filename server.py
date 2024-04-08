d3cf0364d9be05c7dc9b1a813f215dcd87ca5b57
Matt Owens <mowens3@gmail.com>
2024-04-07 19:10:59 -0700

    update build

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ import http.server
 import socketserver
 import start_app
 import start_bot
+from commissar.app import app
+from commissar import ConfigLoader
 
 from http import HTTPStatus
 from commissar.app import app
-
+cfg = ConfigLoader().config
 
 
 start_app.app.start()
