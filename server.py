fb94131b8430a63c12094faf783ca183a8942685
Matt Owens <mowens3@gmail.com>
2024-04-08 23:51:22 -0700

    config and template updates

--- server.py
+++ server.py
@@ -6,10 +6,3 @@ import start_bot
 from commissar.app import app
 from commissar import ConfigLoader
 
-from http import HTTPStatus
-from commissar.app import app
-cfg = ConfigLoader().config
-
-
-# start_app.app.start()
-# start_bot.bot.start()
